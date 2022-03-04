250aa31bfdfb8b61b54aa5331f6451bf122bee9e
19khs <94922290+19khs@users.noreply.github.com>
2022-03-04 21:16:56 +0000

    Update TTA week 11 Homework.sql

--- TTA week 11 Homework.sql
+++ TTA week 11 Homework.sql
@@ -2,8 +2,7 @@ CREATE DATABASE StaffList;
 
 create table    Staff_Details (
     StaffNo int,
-    Name varchar(40),
-    Title enum('Mr','Ms','Mrs'),
+    Name varchar(30),
     Age int,
     StartDate date,
     Grade char(1),
@@ -12,10 +11,10 @@ create table    Staff_Details (
   	PRIMARY KEY (Name),
 	UNIQUE (EmpID)
 );
-insert into Staff_Details (StaffNo, Name, Title, Age, StartDate, Grade, Salary)
-values 	(3, "Freddie", "Mr", 25, "2020-07-01", "C", 24000),
-		(2, "Lucy", "Mr",26,"2018-01-02","B",34400),
-		(1, "Jamie", "Mr", 43, "2015-08-05","A",62000),
+insert into Staff_Details (StaffNo, Name, Age, StartDate, Grade, Salary)
+values 	(3, "Freddie", 25, "2020-07-01", "C", 24000),
+		(2, "Lucy",26,"2018-01-02","B",34400),
+		(1, "Jamie", 43, "2015-08-05","A",62000),
 		
 
 EXPLAIN StaffList;
